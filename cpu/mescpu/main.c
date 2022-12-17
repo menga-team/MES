75751832817695d9f3ac56ce78161e9c72463ec5
yegvla <yegvla@proton.me>
2022-12-17 12:40:13 +0100

    don't compile controller

--- cpu/mescpu/main.c
+++ cpu/mescpu/main.c
@@ -4,24 +4,19 @@
 #include "main.h"
 #include "time.h"
 #include "gpu.h"
-#include "controller.h"
+//#include "controller.h"
 #include "sdcard.h"
 
 int main(void) {
         rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
         clock_peripherals();
         configure_io();
-        controller_configure_io();
+        //controller_configure_io();
         configure_systick();
-        controller_setup_timers();
+        //controller_setup_timers();
         gpu_initiate_communication();
         gpu_block_until_ready();
         sdcard_init_peripheral();
-        controller_get_button_by_index(2);
-        controller_get_button_by_controller_and_index(0, 2);
-        controller_get_buttons(0);
-        controller_get_status(0);
-        controller_get_statuses();
         while (true);
 }
 
